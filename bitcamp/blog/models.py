6c1d2eefda458cb07696290b34b87bd4190f05a3
gio1971 <giooorgi7@gmail.com>
2022-01-19 02:19:08 +0400

    addes Post models with image, comments models also

--- bitcamp/blog/models.py
+++ bitcamp/blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse, reverse_lazy
 from accounts.models import Account
-
 
 
 class Category(models.Model):
     category_name = models.CharField(max_length=124, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
     describtion = models.TextField(blank=True)
-    cat_image = models.ImageField(upload_to='photo/categories', blank=True)
+    cat_image = models.ImageField(upload_to='category', blank=True)
 
     class Meta:
         verbose_name = 'category'
@@ -24,5 +24,24 @@ class Post(models.Model):
     description = models.CharField(max_length=244)
     text = models.TextField()
     category = models.ManyToManyField(Category)
-    image = models.ImageField(upload_to='images/post')
-    create_date = models.DateTimeField(default=timezone.now())
+    image = models.ImageField(upload_to='post')
+    create_date = models.DateTimeField(default=timezone.now)
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(Account, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text
--- bitcamp/blog/views.py
+++ bitcamp/blog/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 # Create your views here.
+from django.shortcuts import get_object_or_404, get_list_or_404
+from . models import *
 
 
 def home(request):
@@ -8,9 +11,24 @@ def home(request):
 
 
 def posts(request):
-    list = ['post_1', 'post_2', 'post_3', 'post_4']
-    return render(request, 'blog/post_list.html', {'list':list})
+    posts = get_list_or_404(Post)
+    # context = {'posts':posts}
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
 
 def post(request, pk):
-    return render(request, 'post_detail.html')
+    post = get_object_or_404(Post, pk=pk)
+    print(post)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+
+def comment_detail(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    return render(request, 'blog/comment_detail.html', {'comment':comment})
+
+
+def comment_list(request):
+    comments = get_list_or_404(Comment)
+    # return HttpResponse('This is comment list')
+    context = {'posts':posts}
+    return render(request, 'blog/comment_list.html', {'comments':comments})
