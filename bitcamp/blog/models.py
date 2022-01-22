7ba6cb24668fcde30ecedf07ed0f71d68a28cb87
Pirmsho <d.pirmisashvili@gmail.com>
2022-01-22 18:51:09 +0100

    started post_detail styling

--- bitcamp/blog/models.py
+++ bitcamp/blog/models.py
@@ -25,7 +25,6 @@ class Category(models.Model):
         return reverse('home')
 
 
-
 class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
@@ -34,19 +33,20 @@ class Post(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     category = models.ManyToManyField(Category)
     image = models.ImageField(upload_to='post')
-    
+
     def get_absolute_url(self):
-        return reverse("blog:post", kwargs={'pk':self.pk})
+        return reverse("blog:post", kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         ordering = ['-create_date']
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(Account, on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
