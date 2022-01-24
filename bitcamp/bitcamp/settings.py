68a529957a4741e1798ca1f07b1ae14d6c65bde5
gio1971 <giooorgi7@gmail.com>
2022-01-24 21:07:04 +0400

    Static folder placed in BASE_DIR/static, no other static folders yet. all posts are in one colomn

--- bitcamp/bitcamp/settings.py
+++ bitcamp/bitcamp/settings.py
@@ -118,11 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static_common'
 STATICFILES_DIRS = [
-    'bitcamp/static',
+    BASE_DIR / 'static',
 ]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
--- bitcamp/user/urls.py
+++ bitcamp/user/urls.py
@@ -5,13 +5,13 @@ from .views import *
 
 app_name='user'
 urlpatterns = [
-    path('login/', my_login, name='login'),
-    path('logout/', logout_view, name='logout'),
-
     path('', AutorListView.as_view(), name='authors'),
-    # path('new/', CreateAuthorView.as_view(), name='create_author'),
-    path('neww/', register, name='register'),
     path('<int:pk>/', AuthorDetailView.as_view(), name='author'),
     path('<int:pk>/update/', UpdateAuthorView.as_view(), name='author_update'),
     path('<int:pk>/delete/', AuthorDeleteView.as_view(), name='author_delete'),
+
+    path('login/', my_login, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register, name='register'),
+
 ]
