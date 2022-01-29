0a7a083adb35d06b47f2db978702ff7d93ac904c
gio1971 <giooorgi7@gmail.com>
2022-01-30 02:31:48 +0400

    added confirm password

--- bitcamp/user/views.py
+++ bitcamp/user/views.py
@@ -38,58 +38,39 @@ class AuthorDeleteView(DeleteView):
     success_url = reverse_lazy('user:authors')
 
 
-# @@@@@@@@@@@@@@@@@ დეკორატორი ჭირდება რომ მხოლოდ ადმინმა შეძლოს მენტორის დარეგისტრირება
 def register(request):
     registered = False
     if request.method == 'POST':
-        # Get info from "both" forms
-        # It appears as one form to the user on the .html page
         user_form = AuthorForm(data=request.POST)
-        # profile_form = UserProfileInfoForm(data=request.POST)
-        # Check to see both forms are valid
+        print(user_form)
         if user_form.is_valid():
-            # Save User Form to Database
             user = user_form.save()
-            # Hash the password
-            user.set_password(user.password)
-            # Update with Hashed password
+            # user.set_password(user.password1)
             user.save()
-            # Now we deal with the extra info!
-            # Can't commit yet because we still need to manipulate
-            # profile = profile_form.save(commit=False)
-            # Set One to One relationship between
-            # UserForm and UserProfileInfoForm
-            # profile.user = user
-            # Check if they provided a profile picture
             if 'image' in request.FILES:
-                print('found it')
-                # If yes, then grab it from the POST form reply
                 user.image = request.FILES['image']
-            # Now save model
             user.save()
             # Registration Successful!
             registered = True
             return HttpResponseRedirect(reverse('user:login'))
         else:
-            # One of the forms was invalid if this else gets called.
             print(user_form.errors)
     else:
-        # Was not an HTTP post so we just render the forms as blank.
         user_form = AuthorForm()
-        # profile_form = UserProfileInfoForm()
-    # This is the render and context dictionary to feed
-    # back to the registration.html file page.
     return render(request,'user/author_form.html',
                           {'form':user_form,
-                        #    'profile_form':profile_form,
                            'registered':registered})
 
 
 
 def my_login(request):
     if request.method == 'POST':
+        print(request.user)
+        print(request.POST)
         username = request.POST['username']
+        print(username)
         password = request.POST['password']
+        print(password)
         user = authenticate(request, username=username, password=password)
         print(user)
         if user:
